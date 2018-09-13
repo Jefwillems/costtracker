a205c104974f769a51dd9b423201f949a314ee52
Jefwillems <jefwillems100@gmail.com>
2018-09-13 10:12:21 -0400

    init + setup config files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'costtracker.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true, }),
+  ],
+};
